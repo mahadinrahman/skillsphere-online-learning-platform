aeed20480d2f3c7b0e86269ff067d2a4174d82c5
mahadinrahman <mahadinrahman333@gmail.com>
2026-05-06 02:06:18 +0600

    search added

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,17 +1,44 @@
+'use client'
 import AllCards from '@/components/AllCards';
-import React from 'react';
+import { use, useState } from 'react';
+import {Button, Label, SearchField} from "@heroui/react";
 
-const AllCoursesPage = async () => {
+    const dataPromise =  fetch('https://skillsphere-online-learning-platfor-topaz.vercel.app/data.json').then(res=> res.json());
 
-    const res = await fetch('https://skillsphere-online-learning-platfor-topaz.vercel.app/data.json');
-    const datas = await res.json();
+const AllCoursesPage = () => {
+
+   const datas=use(dataPromise);
+
+      const [filteredCourses, setFilteredCourses] = useState(datas);
+      const [search,setSearch]=useState('');
+  
+      const handleSearch=()=>{
+    const result=datas.filter(data=>data.title.toLowerCase().includes(search.toLowerCase())
+   )
+   setFilteredCourses(result);
+
+  }
 
     return (
         <div className='max-w-11/12 mx-auto'>
             <h2 className='text-3xl font-bold text-center mt-15 text-fuchsia-900'>All Courses</h2>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-3 mt-13'>
+             <SearchField name="search" className="w-90 mx-auto my-5">
+                <Label>Search Courses</Label>
+                <SearchField.Group>
+                    <SearchField.SearchIcon />
+                    <SearchField.Input className=" " placeholder="Search..." type="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+
+                    <SearchField.ClearButton />
+                    <Button variant="outline" onClick={handleSearch}>
+                        Search
+                    </Button>
+                </SearchField.Group>
+            </SearchField>
+           <div className='grid grid-cols-1 md:grid-cols-3 gap-3 mt-13'>
+
+
                 {
-                    datas.map(data => <AllCards key={data.id} data={data}></AllCards>)
+                    filteredCourses.map(data => <AllCards key={data.id} data={data} ></AllCards>)
                 }
             </div>
         </div>
